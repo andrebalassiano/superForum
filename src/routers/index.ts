809d83cf0092e074b838ecb4309a58388881bb27
Andre Balassiano <andrebalassiano@gmail.com>
2026-03-16 18:44:26 -0700

    Set up app/server and add posts router endpoints

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+
 import postsRouter from '../modules/posts/posts.router';
+// import authRouter from '../modules/auth/auth.router';
+// import commentsRouter from '../modules/comments/comments.router';
+// import subredditsRouter from '../modules/subreddits/subreddits.router';
+// import votesRouter from '../modules/votes/votes.router';
 
 const router = express.Router();
 
-// wire from each module
 router.use('/posts', postsRouter);
+// router.use('/auth', authRouter);
+// router.use('/comments', commentsRouter);
+// router.use('/subreddits', subredditsRouter);
+// router.use('/votes', votesRouter);
 
 
 
